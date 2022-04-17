3d7eac79aa7c4f1fa7dd00df9b8f5fe4f5df4b0b
La-catedral <53202870+La-catedral@users.noreply.github.com>
2022-04-17 22:16:24 +0800

    public commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+库环境：Python 3.9
+操作系统：macOS 11.3
+
+文档说明：
+  input文件夹
+    文法文件：my_grammar.txt
+    待编译程序：lexical_input.c
+  output文件夹
+    语法树结果：out.txt
+    分析表：table.txt
+    语法分析过程信息：syntax_lst.txt
+
+运行方法：
+  在当前目录下执行 python Parser.py
+  如需指定待编译c文件，直接在上述命令行添加参数即可
---
+++ src/input/lexical_input.c
@@ -0,0 +1,38 @@
+int a;
+float b;
+char c;
+char d[2][0x2];
+
+struct student {
+    int a;
+    float b; };
+
+char do_something (int a , int b , int c) {
+    char i;
+    i = d[1][1];
+}
+
+b =  3;
+d[1][1] = "hello";
+c = d[1][0];
+
+int a
+bool c;
+
+while ( a >= b )
+{
+    b = b + 1;
+    if ( b == 0 )
+    {
+    b = d[0][1];
+    }
+    else
+    {
+    a = 1;
+    }
+}
+
+a = 1 * 2 *;
+b = 1 + 2 ;
+
+do_something(a+d[1][0], 10*12, 1.0e-3);
